51f6409dadd210fe1ad27232e11f9414806de7f1
Mazy <mazy_ios@163.com>
2017-10-26 11:27:24 +0800

    feat： 添加 html/加载 html/ 完成 js func 监听

--- iOS_JS_InteractByUIWebView/iOS_JS_InteractByUIWebView/ViewController.swift
+++ iOS_JS_InteractByUIWebView/iOS_JS_InteractByUIWebView/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    fileprivate var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        webView = UIWebView(frame: UIScreen.main.bounds)
+        webView.delegate = self
+        
+        let htmlURL = Bundle.main.url(forResource: "Index.html", withExtension: nil)
+        let request = URLRequest(url: htmlURL!)
+    
+        // 如果不想要webView 的回弹效果
+        webView.scrollView.bounces = false
+        // UIWebView 滚动的比较慢，这里设置为正常速度
+        webView.scrollView.decelerationRate = UIScrollViewDecelerationRateNormal
+        
+        // 请求数据
+        webView.loadRequest(request)
+    
+        view.addSubview(webView)
+        
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UIWebViewDelegate {
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        print("webViewDidStartLoad")
     }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        print("webViewDidFinishLoad")
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print("didFailLoadWithError \(error)")
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        guard let url = request.url, let scheme = url.scheme else {
+            return false
+        }
+        if scheme == "iOSAction" {
+            self.handleCustomAction(url: url)
+            return false
+        }
+        return true
+    }
+}
 
-
+extension ViewController {
+    
+    func handleCustomAction(url: URL) {
+        guard let host = url.host else {
+            return
+        }
+        print(host)
+    }
 }
 
