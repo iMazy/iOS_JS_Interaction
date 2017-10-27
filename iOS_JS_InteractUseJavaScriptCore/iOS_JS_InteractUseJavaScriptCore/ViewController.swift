35ea253ce918b5f3369e0197ea36a814d8d4bb77
Mazy <mazy_ios@163.com>
2017-10-27 14:03:50 +0800

    feat: 完成 jjavascript 的交互

--- iOS_JS_InteractUseJavaScriptCore/iOS_JS_InteractUseJavaScriptCore/ViewController.swift
+++ iOS_JS_InteractUseJavaScriptCore/iOS_JS_InteractUseJavaScriptCore/ViewController.swift
@@ -8,55 +8,135 @@
 
 import UIKit
 import WebKit
+import JavaScriptCore
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    fileprivate var webView: WKWebView!
+    fileprivate var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
-        let configuration:WKWebViewConfiguration = WKWebViewConfiguration()
-        configuration.userContentController = WKUserContentController()
+        webView = UIWebView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 300))
+        webView.delegate = self
         
-        let preferences: WKPreferences = WKPreferences()
-        preferences.javaScriptCanOpenWindowsAutomatically = true
-        preferences.minimumFontSize = 30
-        configuration.preferences = preferences
+        let htmlURL = Bundle.main.url(forResource: "index.html", withExtension: nil)
+        let request = URLRequest(url: htmlURL!)
+    
+        // 如果不想要webView 的回弹效果
+//        webView.scrollView.bounces = false
+        // UIWebView 滚动的比较慢，这里设置为正常速度
+        webView.scrollView.decelerationRate = UIScrollViewDecelerationRateNormal
         
-        self.webView = WKWebView(frame:CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 300), configuration: configuration)
+        // 请求数据
+        webView.loadRequest(request)
         
-        let url = Bundle.main.url(forResource: "index.html", withExtension: nil)!
-        self.webView.loadFileURL(url, allowingReadAccessTo: url)
-        
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        
-        self.view.addSubview(webView)
+        view.addSubview(webView)
     }
 }
 
-
-extension ViewController: WKUIDelegate, WKNavigationDelegate {
+extension ViewController: UIWebViewDelegate {
     
-    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
-        let alertVC = UIAlertController(title: "温馨提示", message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "知道了", style: .cancel, handler: { (_) in
-            completionHandler()
-        }))
-        self.present(alertVC, animated: true, completion: nil)
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        addCustomActions()
+    }
+}
+
+extension ViewController {
+    
+    fileprivate func addCustomActions() {
+        guard let context = self.webView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as? JSContext else { return }
+        addShareWithContext(context)
+        addScanWithContext(context)
+        addLocationWithContext(context)
+        addSetBGColorWithContext(context)
+        addPayActionWithContext(context)
+        addShakeActionWithContext(context)
+        addGoBackWithContext(context)
     }
     
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        guard let url = navigationAction.request.url, let scheme = url.scheme else {
-            decisionHandler(.allow)
-            return
-        }
-        if scheme == "iosaction" {
-            decisionHandler(.cancel)
-            return
-        }
-        decisionHandler(.allow)
+    func addScanWithContext(_ context: JSContext) {
         
+        let callBack : @convention(block) (AnyObject?) -> Void = {_ in
+            print("扫一扫")
+        }
+        
+        context.setObject(unsafeBitCast(callBack, to: AnyObject.self), forKeyedSubscript: "scan" as NSCopying & NSObjectProtocol)
+        
+    }
+    
+    func addLocationWithContext(_ context: JSContext) {
+        let callBack : @convention(block) (AnyObject?) -> Void = { _ in
+            //    Waiting
+            let jsStr = "setLocation('\("湖北省—武汉市")')"
+            JSContext.current().evaluateScript(jsStr)
+        }
+        
+        context.setObject(unsafeBitCast(callBack, to: AnyObject.self), forKeyedSubscript: "getLocation" as NSCopying & NSObjectProtocol)
+    }
+    
+    func addSetBGColorWithContext(_ context: JSContext) {
+        let callBack : @convention(block) (AnyObject?) -> Void = { [weak self] (paramFromJS) -> Void in
+            //    Waiting
+            let value = JSContext.currentArguments() as! [JSValue]
+            let r = value[0].toDouble()
+            let g = value[1].toDouble()
+            let b = value[2].toDouble()
+            let a = value[3].toDouble()
+            DispatchQueue.main.sync {
+                self?.view.backgroundColor = UIColor(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0), alpha: CGFloat(a))
+            }
+        }
+        
+        context.setObject(unsafeBitCast(callBack, to: AnyObject.self), forKeyedSubscript: "setColor" as NSCopying & NSObjectProtocol)
+        
+    }
+    
+    func addShareWithContext(_ context: JSContext) {
+        
+        let callBack : @convention(block) (AnyObject?) -> Void = {_ in
+            //    Waiting
+            let value = JSContext.currentArguments() as! [JSValue]
+            let title = value[0].toString() ?? ""
+            let content = value[1].toString() ?? ""
+            let url = value[2].toString() ?? ""
+            let jsStr = "shareResult('\(title)','\(content)', '\(url)')"
+            JSContext.current().evaluateScript(jsStr)
+        }
+        
+        context.setObject(unsafeBitCast(callBack, to: AnyObject.self), forKeyedSubscript: "share" as NSCopying & NSObjectProtocol)
+    }
+    
+    func addPayActionWithContext(_ context: JSContext) {
+        let callBack : @convention(block) (AnyObject?) -> Void = { _ in
+            
+            let value = JSContext.currentArguments() as! [JSValue]
+            let orderNo = value[0].toString() ?? ""
+            let channel = value[1].toString() ?? ""
+            let amount = value[2].toString() ?? ""
+            let subject = value[3].toString() ?? ""
+            let jsStr = "payResult('\(orderNo)','\(channel)', '\(amount)', '\(subject)')"
+            JSContext.current().evaluateScript(jsStr)
+        }
+        
+        context.setObject(unsafeBitCast(callBack, to: AnyObject.self), forKeyedSubscript: "payAction" as NSCopying & NSObjectProtocol)
+    }
+    
+    func addShakeActionWithContext(_ context: JSContext) {
+        let callBack : @convention(block) (AnyObject?) -> Void = { _ in
+            AudioServicesPlaySystemSound (kSystemSoundID_Vibrate);
+        }
+        
+        context.setObject(unsafeBitCast(callBack, to: AnyObject.self), forKeyedSubscript: "shake" as NSCopying & NSObjectProtocol)
+    }
+    
+    func addGoBackWithContext(_ context: JSContext) {
+        let callBack : @convention(block) (AnyObject?) -> Void = { [weak self] (paramFromJS) -> Void in
+            self?.webView.goBack()
+        }
+        
+        context.setObject(unsafeBitCast(callBack, to: AnyObject.self), forKeyedSubscript: "goBack" as NSCopying & NSObjectProtocol)
     }
 }
